aa9a84dfa6f4b486c96c53844fcfd344b04d40ed
Unknown <audreyrose.lb@live.fr>
2018-02-20 21:52:50 -0500

    ajout de la méthode post pour ajouter

--- 01_app.js
+++ 01_app.js
@@ -33,3 +33,12 @@ app.get('/', (req, res) => {
 	}) 
 })
 
+
+app.post('/ajouter', (req, res) => {
+	db.collection('adresse').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+		console.log('sauvegarder dans la BD')
+		res.redirect('/')
+	})
+})
+
