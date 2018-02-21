ff18789dfd2675c84b5fb70592f003f284394a3b
Unknown <audreyrose.lb@live.fr>
2018-02-21 11:57:07 -0500

    redirection vers l'accueil

--- 01_app.js
+++ 01_app.js
@@ -50,6 +50,7 @@ app.post('/modifier', (req, res) => {
 	db.collection('adresse').save(req.body, (err, result) => {
 		if (err) return console.log(err)
 		console.log('sauvegarder dans la BD')
+		res.redirect('/')
 	})
 })
 
