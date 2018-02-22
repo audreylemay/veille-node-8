4e903c923d37773455d0e759bfc91b1774701c10
Unknown <audreyrose.lb@live.fr>
2018-02-21 21:19:59 -0500

    ajout des tableaux email et telephone

--- mes_modules/peupler/tableaux.js
+++ mes_modules/peupler/tableaux.js
@@ -17,3 +17,19 @@ const tabPrenom = [
 	"Audrey",
 	"Leorio"
 ]
+
+const tabTel = [
+	"450-666-6666",
+	"123-456-7890",
+	"666-666-6666",
+	"420-420-4204"
+	"514-515-5657"
+]
+
+const tabEmail = [
+	"lilb@bb.com",
+	"boy@boy.com",
+	"burger@blchr.com"
+	"strgr@thngs.com",
+	"lr@hxh.com"
+]
