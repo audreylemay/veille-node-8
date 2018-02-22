dc900781f4370c454a5e7966196517113bc66c25
Unknown <audreyrose.lb@live.fr>
2018-02-21 22:03:32 -0500

    génération aléatoire d'adresse

---
+++ mes_modules/peupler/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const tableau = require("./tableaux.js");
+
+console.log(tableau.tabNom);
+console.log(tableau.tabPrenom);
+console.log(tableau.tabTel);
+console.log(tableau.tabEmail);
+
+const maxNom = tableau.tabNom.length;
+const maxPrenom = tableau.tabPrenom.length;
+const maxTel = tableau.tabTel.length;
+const maxEmail = tableau.tabEmail.length;
+
+const peupler = () => {
+
+	console.log("fonction peupler");
+
+	let positionNom = Math.floor(Math.random() * maxNom);
+	let nom = tableau.tabNom[positionNom];
+
+	let positionPrenom = Math.floor(Math.random() * maxPrenom);
+	let prenom = tableau.tabPrenom[positionPrenom];
+
+	let positionTel = Math.floor(Math.random() * maxTel);
+	let tel = tableau.tabTel[positionTel];
+
+	let positionEmail = Math.floor(Math.random() * maxEmail);
+	let email = tableau.tabEmail[positionEmail];
+
+	return {
+		nom : nom, 
+		prenom : prenom,
+		tel : tel,
+		email : email
+	}
+
+	return "fin";
+
+}
+
+module.exports = peupler;
